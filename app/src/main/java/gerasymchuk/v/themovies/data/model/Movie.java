80119e5f4918358a90a3c2ea291d4ed665b898b5
vitaliygerasymchuk <vitaliy.herasymchuk@gmail.com>
2018-01-21 13:51:54 +0200

    EDIT: change field List<Integer> (genreIds) to ArrayList<Integer> for room support

--- app/src/main/java/gerasymchuk/v/themovies/data/model/Movie.java
+++ app/src/main/java/gerasymchuk/v/themovies/data/model/Movie.java
@@ -3,6 +3,7 @@ package gerasymchuk.v.themovies.data.model;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import android.arch.persistence.room.TypeConverter;
+import android.arch.persistence.room.TypeConverters;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
