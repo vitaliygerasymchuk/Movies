b767d4ff7e98f3803de2aba6292691a5562768a8
vitaliygerasymchuk <vitaliy.herasymchuk@gmail.com>
2018-01-22 14:52:58 +0200

    ADD: logs to ViewPagerAdapter.java

--- app/src/main/java/gerasymchuk/v/themovies/view/main/view/ViewPagerAdapter.java
+++ app/src/main/java/gerasymchuk/v/themovies/view/main/view/ViewPagerAdapter.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import gerasymchuk.v.themovies.shared.AbsPagerAdapter;
+import gerasymchuk.v.themovies.shared.Logger;
 
 /**
  * Created by vitaliygerasymchuk on 1/12/18
@@ -16,28 +17,61 @@ import gerasymchuk.v.themovies.shared.AbsPagerAdapter;
 
 public class ViewPagerAdapter extends AbsPagerAdapter {
 
+    @SuppressWarnings("unused")
+    private static final String TAG = "ViewPagerAdapter";
+
+    @SuppressWarnings("unused")
+    private static final boolean DEBUG = true;
+
     @NonNull
     private List<Fragment> fragments = new ArrayList<>();
 
-    public ViewPagerAdapter(@NonNull FragmentManager fm) {
+    ViewPagerAdapter(@NonNull FragmentManager fm) {
         super(fm);
     }
 
     @Override
     public Fragment getItem(int position) {
+        return getFragmentForPosition(position);
+    }
+
+    @Override
+    public int getCount() {
+        log("getCount :: %s ", String.valueOf(fragments.size()));
+        return fragments.size();
+    }
+
+    /**
+     * Gets fragment for provided position or new Fragment() if position is >= fragments.size()
+     *
+     * @param position position
+     * @return Fragment
+     */
+    @NonNull
+    private Fragment getFragmentForPosition(int position) {
         if (position < fragments.size()) {
             return fragments.get(position);
         }
         return new Fragment();
     }
 
-    @Override
-    public int getCount() {
-        return fragments.size();
-    }
-
-    public void addFragments(@NonNull Fragment... fragment) {
+    /**
+     * Adds fragments to {@link ViewPagerAdapter}
+     *
+     * @param fragment {@link Fragment}
+     */
+    void addFragments(@NonNull Fragment... fragment) {
+        log("addFragments :: %s ", String.valueOf(fragment.length));
         fragments.addAll(Arrays.asList(fragment));
         notifyDataSetChanged();
+    }
+
+    /**
+     * Log
+     */
+    private void log(@NonNull String msg, Object... args) {
+        if (DEBUG) {
+            Logger.d(TAG, msg, args);
+        }
     }
 }
