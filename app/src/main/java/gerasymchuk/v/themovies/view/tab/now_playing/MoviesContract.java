09d7dc547942c63b9dd826d8882d16cb16a1a4ab
vitaliygerasymchuk <vitaliy.herasymchuk@gmail.com>
2018-01-23 12:30:26 +0200

    EDIT: change list to paged list

--- app/src/main/java/gerasymchuk/v/themovies/view/tab/now_playing/MoviesContract.java
+++ app/src/main/java/gerasymchuk/v/themovies/view/tab/now_playing/MoviesContract.java
@@ -1,6 +1,8 @@
 package gerasymchuk.v.themovies.view.tab.now_playing;
 
+import android.arch.paging.PagedList;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import java.util.List;
 
@@ -16,10 +18,12 @@ public interface MoviesContract {
     interface Presenter extends BasePresenter {
         void onVisibilityChanged(boolean isVisible);
 
+        void checkMovies(@Nullable PagedList<Movie> movies);
+
         void getMoreMovies();
     }
 
     interface View {
-        void renderMovies(@NonNull List<Movie> movieList);
+        void renderMovies(@NonNull PagedList<Movie> movieList);
     }
 }
