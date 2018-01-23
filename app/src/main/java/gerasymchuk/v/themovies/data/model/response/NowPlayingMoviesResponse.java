1019daa16266f6f349c900eaf64dcb0b9f850fcc
vitaliygerasymchuk <vitaliy.herasymchuk@gmail.com>
2018-01-23 11:51:23 +0200

    ADD: page, totalItems, totalPages values to NowPlayingMoviesResponse.java

--- app/src/main/java/gerasymchuk/v/themovies/data/model/response/NowPlayingMoviesResponse.java
+++ app/src/main/java/gerasymchuk/v/themovies/data/model/response/NowPlayingMoviesResponse.java
@@ -16,10 +16,22 @@ public class NowPlayingMoviesResponse {
     @SerializedName("results")
     public List<Movie> movies = new ArrayList<>();
 
+    @SerializedName("page")
+    public int page;
+
+    @SerializedName("total_results")
+    public int totalItems;
+
+    @SerializedName("total_pages")
+    public int totalPages;
+
     @Override
     public String toString() {
         return "NowPlayingMoviesResponse{" +
-                "movies=" + movies.size() +
+                "movies=" + movies.size() + ", " +
+                "page=" + page + ", " +
+                "totalItems=" + totalItems + ", " +
+                "totalPages=" + totalPages + ", " +
                 '}';
     }
 }
