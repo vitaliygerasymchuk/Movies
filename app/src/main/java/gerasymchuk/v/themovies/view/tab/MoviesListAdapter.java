53a5d0d5e1dfb16db6833d6415d7cb0746800fa5
vitaliygerasymchuk <vitaliy.herasymchuk@gmail.com>
2018-01-24 22:16:24 +0200

    ADD: callbacks for movie item clicked event

--- app/src/main/java/gerasymchuk/v/themovies/view/tab/MoviesListAdapter.java
+++ app/src/main/java/gerasymchuk/v/themovies/view/tab/MoviesListAdapter.java
@@ -2,6 +2,7 @@ package gerasymchuk.v.themovies.view.tab;
 
 import android.arch.paging.PagedListAdapter;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.recyclerview.extensions.DiffCallback;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -21,24 +22,40 @@ import gerasymchuk.v.themovies.shared.Logger;
 
 public class MoviesListAdapter extends PagedListAdapter<Movie, MoviesListAdapter.MovieItemHolder> {
 
+    public interface Callback {
+        void onMovieClicked(@NonNull Movie movie);
+    }
+
     @SuppressWarnings("unused")
     public static final boolean DEBUG = true;
 
     @SuppressWarnings("unused")
     private static final String TAG = "MoviesListAdapter";
 
-    MoviesListAdapter() {
+    @Nullable
+    private Callback callback;
+
+    MoviesListAdapter(@NonNull Callback callback) {
         super(DIFF_CALLBACK);
+        this.callback = callback;
     }
 
     @Override
     public MovieItemHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new MovieItemHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_movie, parent, false));
+        return new MovieItemHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_movie, parent, false), adapterPosition -> {
+            if (callback != null) {
+                Movie movie = getItem(adapterPosition);
+                if (movie != null) {
+                    callback.onMovieClicked(movie);
+                }
+            }
+        });
     }
 
     @Override
     public void onBindViewHolder(MovieItemHolder holder, int position) {
         final Movie movie = getItem(position);
+        Logger.d(TAG, "onBindViewHolder :: position " + position + " item " + movie);
         if (movie != null) {
             holder.bind(movie);
         }
@@ -54,7 +71,6 @@ public class MoviesListAdapter extends PagedListAdapter<Movie, MoviesListAdapter
         @Override
         public boolean areItemsTheSame(@NonNull Movie oldMovie, @NonNull Movie newMovie) {
             // User properties may have changed if reloaded from the DB, but ID is fixed
-            log("areItemsTheSame oldMovie %s newMovie %s ", oldMovie.getOriginalTitle(), newMovie.getOriginalTitle());
             return oldMovie.id == newMovie.id;
         }
 
@@ -62,24 +78,26 @@ public class MoviesListAdapter extends PagedListAdapter<Movie, MoviesListAdapter
         public boolean areContentsTheSame(@NonNull Movie oldMovie, @NonNull Movie newMovie) {
             // NOTE: if you use equals, your object must properly override Object#equals()
             // Incorrectly returning false here will result in too many animations.
-            log("areContentsTheSame oldMovie %s newMovie %s ", oldMovie.getOriginalTitle(), newMovie.getOriginalTitle());
             return oldMovie.equals(newMovie);
-        }
-
-        private void log(String msg, Object... args) {
-            if (DEBUG) {
-                Logger.d("DiffCallback", msg, args);
-            }
         }
     };
 
+    /**
+     * {@link android.support.v7.widget.RecyclerView.ViewHolder} which represents {@link Movie}
+     */
     static class MovieItemHolder extends RecyclerView.ViewHolder {
+
+        interface OnMovieClickedCallback {
+            void onAdapterPosition(int adapterPosition);
+        }
 
         TextView tv;
 
-        MovieItemHolder(View itemView) {
+        MovieItemHolder(View itemView, @NonNull OnMovieClickedCallback callback) {
             super(itemView);
             tv = itemView.findViewById(R.id.tv);
+            itemView.findViewById(R.id.card_view)
+                    .setOnClickListener(view -> callback.onAdapterPosition(getAdapterPosition()));
         }
 
         void bind(@NonNull Movie movie) {
@@ -87,6 +105,9 @@ public class MoviesListAdapter extends PagedListAdapter<Movie, MoviesListAdapter
         }
     }
 
+    /**
+     * Log
+     */
     private void log(String msg, Object... args) {
         if (DEBUG) {
             Logger.d(TAG, msg, args);
