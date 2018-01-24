948b746a4986fa0b181092422be6ae3c6429e95e
vitaliygerasymchuk <vitaliy.herasymchuk@gmail.com>
2018-01-24 22:31:34 +0200

    DELETE: unused import

--- app/src/main/java/gerasymchuk/v/themovies/view/tab/AbsMoviesFragment.java
+++ app/src/main/java/gerasymchuk/v/themovies/view/tab/AbsMoviesFragment.java
@@ -1,7 +1,6 @@
 package gerasymchuk.v.themovies.view.tab;
 
 import android.arch.paging.PagedList;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
